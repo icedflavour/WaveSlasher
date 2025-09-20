a24c0482b78b99677c497459a32fc1b795c1f874
Misha <misha.xomen888@gmail.com>
2025-09-20 13:04:55 +0300

    added not working code

---
+++ Assets/scripts/script.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+public class script : MonoBehaviour
+{
+ Rigidbody2D body;
+
+ float horizontal;
+ float vertical;
+
+ public float runSpeed = 20.0f;
+
+ void Start ()
+ {
+   body = GetComponent<Rigidbody2D>(); 
+ }
+
+ void Update ()
+ {
+   horizontal = Input.GetAxisRaw("Horizontal");
+   vertical = Input.GetAxisRaw("Vertical"); 
+ }
+
+ private void FixedUpdate()
+ {  
+   body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+ }
+}
